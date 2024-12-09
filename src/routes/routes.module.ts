dcca7dc71dbda2edaec58074ce33e907881cd305
vhfuly <victor_hugo_fuly@hotmail.com>
2024-12-09 19:22:53 -0300

    feat:  add prisma

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RoutesController],
   providers: [RoutesService],
 })
--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RoutesService {
+  constructor(private prismaService: PrismaService) {}
   create(createRouteDto: CreateRouteDto) {
     return 'This action adds a new route';
   }
